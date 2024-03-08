1d2e592b8eb6fb40ff0973f25b7f4050a1054622
Bruno Alves <alves.bruno945@gmail.com>
2024-03-08 09:53:18 -0300

    Change variable name to `contentView`, add a init and comments functions

--- SunsetAPI/View/Controller/SunsetViewController.swift
+++ SunsetAPI/View/Controller/SunsetViewController.swift
@@ -4,37 +4,49 @@ final class SunsetViewController: UIViewController {
     
     //MARK: - Variables
 
-    private let sunsetView = SunsetView()
+    private let contentView: SunsetViewType
     private let viewModel = SunsetViewModel()
+
+    // MARK: Init
+
+    init(contentView: SunsetViewType = SunsetView()) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     //MARK: - LifeCycle
 
     public override func loadView() {
-        self.view = sunsetView
+        self.view = contentView
     }
     
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        requestingInformation()
+//        requestingInformation()
     }
     
     //MARK: - Functions
 
-    private func requestingInformation() {
-        sunsetView.updateButton.addTarget(self, action: #selector(sendRequest), for: .touchUpInside)
-    }
+//    private func requestingInformation() {
+//        sunsetView.updateButton.addTarget(self, action: #selector(sendRequest), for: .touchUpInside)
+//    }
     
-    @objc private func sendRequest() {
-        viewModel.makeRequesting { information, errorMessage in
-            guard let information = information else {
-                print("Erro")
-                return
-            }
-            DispatchQueue.main.async {
-                self.sunsetView.labelSunrise.text = information.results.sunrise
-                self.sunsetView.labelSunset.text = information.results.sunset
-            }
-        }
-    }
+//    @objc private func sendRequest() {
+//        viewModel.makeRequesting { information, errorMessage in
+//            guard let information = information else {
+//                print("Erro")
+//                return
+//            }
+//            DispatchQueue.main.async {
+//                self.contentView.labelSunrise.text = information.results.sunrise
+//                self.contentView.labelSunset.text = information.results.sunset
+//            }
+//        }
+//    }
 }
 
