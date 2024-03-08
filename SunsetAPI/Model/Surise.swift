f3b026b7462b16c386f1efa042fede6c38f8a3b8
Bruno Alves <alves.bruno945@gmail.com>
2024-03-07 23:56:12 -0300

    Fix name class and add some spaces

--- SunsetAPI/Model/Surise.swift
+++ SunsetAPI/Model/Surise.swift
@@ -1,21 +1,16 @@
-//
-//  Model.swift
-//  SunsetAPI
-//
-//  Created by Bruno Silva on 19/08/21.
-//
-
 import Foundation
 
-struct Surise: Codable {
+struct Sunrise: Codable {
     
     //MARK: - Properties
+
     let results: Results
 }
 
 struct Results: Codable {
-    
+
     //MARK: - Properties
+
     let sunrise: String
     let sunset: String
 }
