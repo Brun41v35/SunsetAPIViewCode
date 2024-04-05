bd579268a7ec9c856903d30b81e0dacc7145b1e0
Bruno Alves <alves.bruno945@gmail.com>
2024-04-04 21:05:06 -0300

    Update `NetworkManager`

--- SunsetAPI/Managers/NetworkManager.swift
+++ SunsetAPI/Managers/NetworkManager.swift
@@ -15,7 +15,38 @@ final class NetworkManager {
 
 extension NetworkManager: NetworkManagerType {
 
-    func loadData() {
-//        let endpoint = baseURL + "?lat=36.7201600&lng=-4.4203400"
+    func loadData(with baseURL: String, completion: @escaping APIResult) {
+
+        let endpoint = baseURL + "?lat=36.7201600&lng=-4.4203400"
+        guard let url = URL(string: endpoint) else { return }
+        let request = URLRequest(url: url)
+
+        let dataTask = session.dataTask(with: request) { data, response, error in
+
+            guard let error = error else {
+                completion(.failure(.unableToComplete))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let decoderResponse = try decoder.decode(Sunrise.self, from: data)
+                completion(.success(decoderResponse))
+            } catch {
+                completion(.failure(.unableToComplete))
+            }
+        }
+        dataTask.resume()
     }
 }
