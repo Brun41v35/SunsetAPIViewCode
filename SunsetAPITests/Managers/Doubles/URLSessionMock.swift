ab0ca08c31918f11aae82818714e8623f0a346dc
Bruno Alves <alves.bruno945@gmail.com>
2024-04-05 23:20:15 -0300

    Add `dataTaskArgsCompletionHandler`

--- SunsetAPITests/Managers/Doubles/URLSessionMock.swift
+++ SunsetAPITests/Managers/Doubles/URLSessionMock.swift
@@ -7,6 +7,7 @@ final class URLSessionMock: URLSessionType {
 
     private(set) var dataTaskCallCount = 0
     private(set) var dataTaskArgsRequest = [URLRequest]()
+    private(set) var dataTaskArgsCompletionHandler = [(Data?, URLResponse?, Error?) -> Void]()
     private let dummyURLSession = DummyURLSessionDataTask()
 
     // MARK: - Methods
@@ -15,6 +16,7 @@ final class URLSessionMock: URLSessionType {
                   completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         dataTaskCallCount += 1
         dataTaskArgsRequest.append(request)
+        dataTaskArgsCompletionHandler.append(completionHandler)
         return dummyURLSession
     }
 }
