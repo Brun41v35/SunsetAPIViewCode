c1b40c49385f28d195e6c99ed9b17970f9a419de
Bruno Alves <alves.bruno945@gmail.com>
2024-04-07 16:57:13 -0300

    Add `SunsetViewControllerType`

--- SunsetAPI/View/SunsetProtocols.swift
+++ SunsetAPI/View/SunsetProtocols.swift
@@ -1,9 +1,15 @@
 import UIKit
 
 protocol SunsetViewModelType {
+    var viewController: SunsetViewControllerType? { get set }
     func loadData()
+}
+
+protocol SunsetViewControllerType: AnyObject {
+    func show(viewModel: SunsetModel)
 }
 
 protocol SunsetViewType where Self: UIView {
     var didTapButton: (() -> Void)? { get set }
+    func show(viewModel: SunsetModel)
 }
