fd322774815551329b6d11cb4b2535c5c82969f7
Bruno Alves <alves.bruno945@gmail.com>
2024-04-06 22:35:05 -0300

    Add `jsonData` and `response` method

--- SunsetAPITests/Managers/NetworkManagerTests.swift
+++ SunsetAPITests/Managers/NetworkManagerTests.swift
@@ -22,4 +22,30 @@ final class NetworkManagerTests: XCTestCase {
 
         XCTAssertEqual(sessionMock.dataTaskArgsRequest.first, request)
     }
+
+    func test_loadData_shouldReturnSuccessResponse() {
+        
+    }
+
+    // MARK: - Helpers
+
+    private func jsonData() -> Data {
+        """
+        {
+          "results": {
+            "sunrise": "5:54:07 AM",
+            "sunset": "6:45:46 PM"
+          },
+        }
+        """.data(using: .utf8)!
+    }
+
+    private func response(with statusCode: Int) -> HTTPURLResponse? {
+        let url = URL(string: "https://dummy.com")!
+        let response = HTTPURLResponse(url: url,
+                                       statusCode: statusCode,
+                                       httpVersion: nil,
+                                       headerFields: nil)
+        return response
+    }
 }
